02c287e3142d82b55ee122ddd69a148148fdbb37
YongBhinKim <coji67@gmail.com>
2022-02-17 16:53:33 +0900

    Toggle버튼 Crew추가

--- Movie/client/src/components/views/MovieDetail/MovieDetail.js
+++ Movie/client/src/components/views/MovieDetail/MovieDetail.js
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from 'react'
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from '../LandingPage/Sections/MainImage';
 import MovieInfo from './Sections/MovieInfo';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
+
 
 function MovieDetail(props) {
 
     let movieId = props.match.params.movieId
     const [Movie, setMovie] = useState([]) // state에 넣기
+    const [Casts, setCasts] = useState([]) // response(콘솔창의 responseForCrew)에서 가져와서 state에 넣기, initial은 배열로 설정
+    // const [Crews, setCrews] = useState([]) // crew 버전 state
+
+    const [ActorToggle_Cast, setActorToggle_Cast] = useState(false)
+    // const [ActorToggle_Crew, setActorToggle_Crew] = useState(false)
 
     useEffect(() => {
         // console.log(props.match) // 634649
@@ -18,12 +26,41 @@ function MovieDetail(props) {
         fetch(endpointInfo)
             .then(response => response.json())
             .then(response => {
-                console.log(response)
-                setMovie(response) // response를 movie state에 넣기 (2)
+                console.log('InfoView1111', response)
+                setMovie(response) // response(= info1111)를 movie state에 넣기 (2)
             })
+
+
+        fetch(endpointCrew)
+            .then(response => response.json())
+            .then(response => {
+                // console.log('responseForCrew1111', response) // 이제 state에 넣자 ! -> const [Cast, setCast]
+                console.log('ViewForCast', response)
+                setCasts(response.cast)// response.cast -> cast만 넣기(crew 제외)
+        })
+
+    //     fetch(endpointCrew)
+    //         .then(response => response.json())
+    //         .then(response => {
+    //             console.log('ViewForCrew1111', response)
+    //             setCasts(response.crew)// response.crew -> crew만 넣기(cast 제외)
+    // })
 
     }, [])
   
+
+    const toggleActorView_Cast = () => {
+        setActorToggle_Cast(!ActorToggle_Cast)
+    }
+    /** 
+    const toggleActorView_Crew = () => {
+        setActorToggle_Crew(!ActorToggle_Crew)
+    }
+    */
+
+
+
+
   // 렌더링
     return (
         <div>
@@ -46,15 +83,61 @@ function MovieDetail(props) {
             />
 
 
-            <br />
+            <br />   {/* 첫 페이지와 비슷하므로 landing page의 movie grid card 에서 코드 가져옴 */}
+            
             {/* Actors Grid */}
+
+            <div>
             <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
-                <button> Toggle Actor View </button>
+                <button onClick={toggleActorView_Cast}> Toggle Cast View </button>
+            </div>
+
+            {/* Cast버전 toggle 버튼 */} {/* 나중에 Crew랑 함수로 빼기 */}
+            {ActorToggle_Cast && 
+            <Row gutter={[16, 16]}> 
+                {Casts && Casts.map((cast, index) => (
+                    <React.Fragment key={index}>
+                        <GridCards
+                            image={cast.profile_path ?
+                                `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                            caracterName={cast.name}
+                        />
+                    </React.Fragment>
+                ))}
+
+            </Row>
+            }
             </div>
 
 
-        </div>
+{/**
+            <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
+                <button onClick={toggleActorView_Crew}> Toggle Crew View </button>
+            </div>
 
+            <div>
+            {/* Crew버전 toggle 버튼 /*
+
+            {ActorToggle_Crew && 
+            <Row gutter={[16, 16]}> 
+                {Crews && Crews.map((crew, index) => (
+                    <React.Fragment key={index}>
+                        <GridCards
+                            image={crew.profile_path ?
+                                `${IMAGE_BASE_URL}w500${crew.profile_path}` : null}
+                            caracterName={crew.name}
+                        />
+                    </React.Fragment>
+                ))}
+
+            </Row>
+            }
+            </div> 
+*/}
+
+
+
+        </div>
         </div>
   )
 }
--- Movie/client/src/components/views/commons/GridCards.js
+++ Movie/client/src/components/views/commons/GridCards.js
@@ -3,15 +3,29 @@ import { Col } from 'antd'; // antd 디자인 사용
 
 // 주소를 가져오기 위해 LandingPage 에서 component 를 가져옴 (props)
 function GridCards(props) {
-  return (
-    <Col lg={6} md={8} xs={24}>
-        <div style={{ position: 'relative' }}> 
-            <a href={`/movie/${props.movieId}`} >
-                <img style={{ width: '100%', height: '200px' }} src={props.image} alt={props.movieName} /> 
-            </a>
-        </div>
-    </Col>
-  )
+
+  if(props.landingPage) {
+    return (
+      <Col lg={6} md={8} xs={24}>
+          <div style={{ position: 'relative' }}> 
+              <a href={`/movie/${props.movieId}`} >
+                  <img style={{ width: '100%', height: '200px' }} src={props.image} alt={props.movieName} /> 
+              </a>
+          </div>
+      </Col>
+    )
+  } else {
+    return (
+      <Col lg={6} md={8} xs={24}>
+          <div style={{ position: 'relative' }}>              
+                  <img style={{ width: '100%', height: '200px' }} src={props.image} alt={props.caracterName} /> 
+          </div>
+      </Col>
+
+    )
+  }
+
+
 }
 
 export default GridCards
